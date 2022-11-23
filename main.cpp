3e5adcc669ff777594292a7eabff0fa6f6a227d3
RicciardiRiccardo <117997389+RicciardiRiccardo@users.noreply.github.com>
2022-11-23 15:19:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main() {
+   int i;
    int n,a=0,b=1,c=0;
+
    cout << "inserisci un numero intero" << endl;
    cin >> n;
    if (n >= 2) {
        cout << b << endl;
-   for (int i = 0; i<n; i++) {
+   for (i = 0; i<n; i++) {
       c = a + b;
       a = b;
       b = c;
